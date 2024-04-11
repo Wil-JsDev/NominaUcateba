ff2a76d5fbf30d3861c550b6436b4434fb8ed179
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-04-11 17:12:14 -0500

    Funcionalidad del boton Agregar profesor

--- UI/NominaProfesores.cs
+++ UI/NominaProfesores.cs
@@ -1,4 +1,5 @@
-﻿using MaterialSkin;
+﻿using BLL;
+using MaterialSkin;
 using MaterialSkin.Controls;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,7 @@ namespace UI
 {
     public partial class FrmNominaProfesores : MaterialForm
     {
-
+        Nomina nomina = new Nomina();
 
         public FrmNominaProfesores()
         {
@@ -23,6 +24,38 @@ namespace UI
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void materialButton2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnCalcularSueldo_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void BtnAgregarProfesor_Click(object sender, EventArgs e)
+        {
+            Profesores profesores = new Profesores();
+
+            profesores.Id = Convert.ToInt16(txtId.Text);
+            profesores.Nombre = Convert.ToString(txtNombre.Text);
+            profesores.Apellido = Convert.ToString(txtApellido.Text);
+            profesores.HorasTrabajadas = Convert.ToDouble(txtHorasTrabjadas.Text);
+            profesores.PrecioHoras = Convert.ToDouble(txtPrecioHoras.Text);
+            nomina.AgregarProfesoresNomina(profesores);
+
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = nomina.GetProfesores();
+            profesores.CalcularSueldo();
+            profesores.CalcularDescuento();
+        }
+
+        private void BtnAgregarProfesor_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
